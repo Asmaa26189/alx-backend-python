e9e30017d7f6232ff2206ec627a2a0b80b1d4060
Asmaa26189 <engasmaahesham@gmail.com>
2024-07-05 02:55:00 +0300

    4-7-2024

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" sum list """
+from typing import Callable, Iterator, Union, Optional, List
+
+
+def sum_list(l: List[float]) -> float:
+    """return int"""
+    return (sum(l))
+
