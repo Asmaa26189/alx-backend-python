41e08f8493aa07e09a8f0d35d11f705c8fc3060a
Asmaa26189 <engasmaahesham@gmail.com>
2024-07-05 03:13:56 +0300

    4-7-2024

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """ to_kv """
 from typing import Callable, Iterator, Union, Optional, List, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple(str, float):
     """ return"""
     return (k, v**2)
